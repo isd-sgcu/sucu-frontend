869e35b3dd4b7754f33a38ea358b8d994092ec42
Chulinuwu <ananzxzoxd@gmail.com>
2024-12-19 19:11:08 +0700

    dasdas

--- src/lib/api/login.ts
+++ src/lib/api/login.ts
@@ -1,23 +1,22 @@
-// src/lib/api.js
-
 const API_URL = import.meta.env.VITE_SERVER_URL;
 
 export async function login(studentId: String, password: String) {
   try {
-    const response = await fetch(API_URL, {
+    const response = await fetch(`${API_URL}/auth/login`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': 'http://localhost:3000'
       },
       body: JSON.stringify({ student_id: studentId, password }),
     });
 
     if (!response.ok) {
-      const errorData = await response.json();
-      throw new Error(errorData.message || 'Failed to login');
+      throw new Error('Login failed');
     }
 
-    return await response.json();
+    const data = await response.json();
+    return data;
   } catch (error) {
     console.error('Login error:', error);
     throw error;
