f7b07fd46a62fbca492e3b5918504ff7e203168e
Chulinuwu <ananzxzoxd@gmail.com>
2024-12-19 19:55:40 +0700

    fix:lint

--- src/lib/api/login.ts
+++ src/lib/api/login.ts
@@ -1,23 +1,23 @@
 const API_URL = import.meta.env.VITE_SERVER_URL;
 
-export async function login(studentId: String, password: String) {
-  try {
-    const response = await fetch(`${API_URL}/api/v1/auth/login`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ student_id: studentId, password }),
-    });
+export async function login(studentId: string, password: string) {
+	try {
+		const response = await fetch(`${API_URL}/api/v1/auth/login`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({ student_id: studentId, password })
+		});
 
-    if (!response.ok) {
-      throw new Error('Login failed');
-    }
+		if (!response.ok) {
+			throw new Error('Login failed');
+		}
 
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error('Login error:', error);
-    throw error;
-  }
+		const data = await response.json();
+		return data;
+	} catch (error) {
+		console.error('Login error:', error);
+		throw error;
+	}
 }
