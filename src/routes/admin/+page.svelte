f4dc7f4b0a1a49955233a618971f05d2a822231f
Chulinuwu <ananzxzoxd@gmail.com>
2024-12-22 19:41:06 +0700

    feat:fetch user to sidebar

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
 	import Sidebar from '$lib/components/Sidebar/Sidebar.svelte';
+	import { getMe } from '$lib/api/getme';
+    import { getDecryptedToken } from '$lib/api/getToken';
+	import { onMount } from 'svelte';
+
 	const menuItems = [
 		{ title: 'ประกาศ', href: '/admin' },
 		{ title: 'เอกสาร', href: '/admin/documents' },
@@ -7,11 +11,31 @@
 		{ title: 'จัดการผู้ใช้งาน', href: '/admin/users' }
 	];
 
-	const user = {
-		name: 'name',
-		lastname: 'lastname',
-		role: 'role'
+	let user = {
+		name: 'Not found',
+		lastname: 'Not found',
+		role: 'Not found',
+		id: 'Not found',
 	};
+
+	onMount(async () => {
+        const token = getDecryptedToken();
+        if (token) {
+            try {
+                const userData = await getMe(token);
+                user = {
+                    name: userData.result.first_name,
+                    lastname: userData.result.last_name,					
+                    role: userData.result.role,
+					id: userData.result.id,
+                };
+            } catch (error) {
+                console.error('Failed to fetch user data:', error);
+            }
+        } else {
+            console.log('No token found');
+        }
+    });
 </script>
 
 <div class="flex min-h-screen bg-white">
