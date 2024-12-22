e3ec1c48206118bf6c2d4a79ded9bc29baa019a5
Sirawit Chanaburanasak <punpunsirawit@gmail.com>
2024-12-22 23:52:02 +0700

    chore: clean up unused dependencies

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,9 @@
 <script>
 	import '../styles/app.css';
-	import { initializeStores } from '@skeletonlabs/skeleton';
-	import { Modal } from '@skeletonlabs/skeleton';
 	import Footer from '$lib/components/Footer/Footer.svelte';
 	import Navbar from '$lib/components/Navbar.svelte';
 
-	// Initialize stores
-	initializeStores();
 </script>
-
-<Modal />
 
 <div>
 	<Navbar />
