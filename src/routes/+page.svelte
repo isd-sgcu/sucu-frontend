e2ea33f5d965a3ae7d2f068f82a1b2dbeae2c9cc
Chulinuwu <ananzxzoxd@gmail.com>
2024-12-22 19:52:21 +0700

    fix:ui landingPage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -36,7 +36,7 @@
 		style="background-image: url({sucuHomeBanner1});"
 	>
 		<div
-			class="w-[650px] h-[263px] max-md:h-[160px] flex flex-col items-center justify-center justify-between"
+			class="w-[650px] h-[263px] max-md:h-[160px] flex flex-col items-center justify-center "
 		>
 			<div
 				class="w-full h-[192px] max-md:h-[120px] p-2.5 flex flex-col gap-2.5 max-md:gap-1 text-sucu-gray-dark"
@@ -45,7 +45,7 @@
 					class={cn(
 						typography({ variant: 'heading1' }),
 						'tracking-tight leading-none',
-						'max-md:text-3xl max-md:font-semibold'
+						'max-md:text-2xl max-md:font-semibold'
 					)}
 				>
 					สโมสรนิสิต
@@ -54,7 +54,7 @@
 					class={cn(
 						typography({ variant: 'heading1' }),
 						'tracking-tight leading-none',
-						'max-md:text-3xl max-md:font-semibold'
+						'max-md:text-2xl max-md:font-semibold'
 					)}
 				>
 					จุฬาลงกรณ์มหาวิทยาลัย
@@ -63,7 +63,7 @@
 					class={cn(
 						typography({ variant: 'body-large' }),
 						'font-semibold leading-6',
-						'max-md:text-xs max-md:leading-3'
+						'max-md:text-xs max-md:leading-3 '
 					)}
 				>
 					Student Union, Chulalongkorn University
@@ -75,7 +75,7 @@
 				class={cn(
 					typography({ variant: 'body-medium' }),
 					'font-semibold px-9 py-2 leading-5',
-					'max-md:text-xs max-md:leading-3 max-md:px-3 max-md:py-1.5'
+					'max-md:text-xs max-md:leading-3 max-md:px-3 max-md:py-1.5 mt-5'
 				)}
 				on:click={() => goto(`/info`)}
 			>
